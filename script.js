e0139898c8a014d9210f6b193fbd7a7cba1a03b9
Nithin KS <50230678+nithinkodadhavadee@users.noreply.github.com>
2023-08-28 13:12:34 +0530

    created clear comman added phone to contact changed the color of the links

--- script.js
+++ script.js
@@ -38,15 +38,50 @@ inputElement.addEventListener('keydown', function(event) {
 
 function processCommand(command) {
     switch (command) {
-        case 'help':
-            return 'Available commands: help, about, contact, resume';
         case 'about':
             return 'This is a terminal emulator like portfolio built using HTML, CSS, and JavaScript.';
+        case 'clear':
+                outputElement.innerHTML = "";
+                return ''
         case 'contact':
-            return 'You can reach me at nithinkodadhavadee@outlook.com.';
+            return `
+            You can reach me at following places: <br> 
+            <a href="mailto:nithinkodadhavadee@outlook.com">nithinkodadhavadee@outlook.com</a> <br> 
+            <a href="tel:+919141128016"> +91 9141128016</a>
+            `;
+        case 'help':
+            return `Available commands: <br>
+            <br>
+            about<br>
+            clear<br>
+            contact<br>
+            help <br>
+            link <br>
+            links<br>
+            resume<br>`;
+        case 'links':
+            return `
+            Use the command <code>link --&ltplatform&gt</code> to be redirected to the following platform.<br>
+            <br>
+            available parameters: <br>
+              --github <br>
+              --linkedin
+            ` ;
+        case command.match(/^link/)?.input:
+            switch(command.slice(7)){
+                case 'github':
+                    window.open("https://github.com/nithinkodadhavadee", "_blank");
+                    return '<a href="https://github.com/nithinkodadhavadee/">The link to my Github Profile</a>';
+                case 'linkedin':
+                    window.open("https://www.linkedin.com/in/nithinkodadhavadee/", "_blank");
+                    return '<a href="https://www.linkedin.com/in/nithinkodadhavadee/">The link to my Linkedin Profile</a>';
+                default:
+                    console.log(command.slice(7));
+                    return 'Un recognized parameter. try <code>links<code> to know how to use the command.';
+            }
         case 'resume':
             window.open('./Resume.pdf', '_blank');
-            return ""
+            return "";
         default:
             return `Command not found: ${command}`;
     }
